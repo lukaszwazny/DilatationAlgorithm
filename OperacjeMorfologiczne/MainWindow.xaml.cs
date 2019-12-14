f1b23305b1d19e781bd7671ea031a947f98ad3bb
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2019-12-14 19:15:04 +0100

    beginning

---
+++ OperacjeMorfologiczne/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace OperacjeMorfologiczne
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage _originalImage;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DodajObraz_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            _originalImage = new BitmapImage(new Uri(openFileDialog.FileName));
+            OriginalImage.Source = _originalImage;
+        }
+    }
+}
